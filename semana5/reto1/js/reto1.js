26e539f96f0763df0cfcaa41ee507bebc2f0e1a3
obitodarck <mayhixccallo@gmail.com>
2022-06-30 01:36:45 -0500

    se agrego un buscador y modal

--- semana5/reto1/js/reto1.js
+++ semana5/reto1/js/reto1.js
@@ -1,161 +1,125 @@
-const laptops = [
-    {
-      marca: "Lenovo",
-      imagen:
-        "https://cdn.cdkeys.com/700x700/media/catalog/product/g/r/grand-theft-auto-iv-complete-edition-pc-steam.jpg",
-      nombre: "Grand Theft Auto V",
-      vendedor: "Falabella",
-      precioOferta: 1599,
-      precioNormal: 3599,
-      calificacion: 2,
-      caracteristicas: {
-        procesador: "i3",
-        tarjetaDeVideo: "integrada",
-        discoDuro: true,
-        discoDuroSolido: "1TB",
-      },
-    },
-    {
-        marca: "Electronic arts",
-        imagen:
-          "https://i.playground.ru/e/J-4wI_L5CHdfBeZzQt48hQ.jpeg",
-        nombre: "fifa 20",
-        vendedor: "Falabella",
-        precioOferta: 1599,
-        precioNormal: 3599,
-        calificacion: 2,
-        caracteristicas: {
-          procesador: "i3",
-          tarjetaDeVideo: "integrada",
-          discoDuro: true,
-          discoDuroSolido: "1TB",
-        },
-      },
-    {
-      marca: "Electronic arts",
-      imagen:
-        "https://cdn.cdkeys.com/700x700/media/catalog/product/s/t/starwars2.jpg",
-      nombre: "Battle Front",
-      vendedor: "Falabella",
-      precioOferta: 3399,
-      precioNormal: 4999,
-      calificacion: 5,
-      caracteristicas: {
-        procesador: "i5",
-        tarjetaDeVideo: "GTX 1650",
-        discoDuro: true,
-        discoDuroSolido: "256gb",
-      },
-    },
-    {
-        marca: "Electronic arts",
-      imagen:
-        "https://pics.filmaffinity.com/tomb_raider-433229795-large.jpg",
-      nombre: "Tomb Raider",
-      vendedor: "Falabella",
-      precioOferta: 3399,
-      precioNormal: 4999,
-      calificacion: 5,
-      caracteristicas: {
-        procesador: "i3",
-        tarjetaDeVideo: "GTX 1650",
-        discoDuro: false,
-        discoDuroSolido: "256gb",
-      },
-    },
-    {
-        marca: "Lenovo",
-        imagen:
-          "https://hedaroart.files.wordpress.com/2016/06/blog-argt.jpg?w=627",
-        nombre: "Call of Duty: Infinite Warfare",
-        vendedor: "Falabella",
-        precioOferta: 1599,
-        precioNormal: 3599,
-        calificacion: 2,
-        caracteristicas: {
-          procesador: "i3",
-          tarjetaDeVideo: "integrada",
-          discoDuro: true,
-          discoDuroSolido: "1TB",
-        },
-      },
-      {
-        marca: "CodeMaster ",
-        imagen:
-          "https://cdn-products.eneba.com/resized-products/rmbtikwjjndtsjtfb0fv_350x200_1x-0.jpg",
-        nombre: "Dirt Rally",
-        vendedor: "Falabella",
-        precioOferta: 3399,
-        precioNormal: 4999,
-        calificacion: 5,
-        caracteristicas: {
-          procesador: "i3",
-          tarjetaDeVideo: "GTX 1650",
-          discoDuro: false,
-          discoDuroSolido: "256gb",
-        },
-      },
 
-      {
-        marca: "Clearbox software ",
-        imagen:
-          "https://cdn.2kgames.com/web/borderlands.com/b3/fob_oak_sup_M_1_huKy5bR.jpg",
-        nombre: "BorderLands3",
-        vendedor: "Falabella",
-        precioOferta: 2690,
-        precioNormal: 4999,
-        calificacion: 5,
-        caracteristicas: {
-          procesador: "i3",
-          tarjetaDeVideo: "GTX 1650",
-          discoDuro: false,
-          discoDuroSolido: "256gb",
-        },
-      },
-
-
-      {
-        marca: "Electronic arts ",
-        imagen:
-          "https://cdn.cdkeys.com/700x700/media/catalog/product/b/a/battlefield-v-pc-get-cheap-cdkeys_5_.jpg",
-        nombre: "Battlefield v",
-        vendedor: "Falabella",
-        precioOferta: 2690,
-        precioNormal: 4999,
-        calificacion: 5,
-        caracteristicas: {
-          procesador: "i3",
-          tarjetaDeVideo: "GTX 1650",
-          discoDuro: false,
-          discoDuroSolido: "256gb",
-        },
-      },
-    ];
   const container=document.querySelector(".container");
-  laptops.map((laptop) => {
-    // ahora en esta seccion vamos a crear los card
-    // Esto es JS se llama template string => ``
-    // Sirve para poder tener texto y variable juntos
-    // return `La laptop se llama ${laptop.nombre} y el precio es ${laptop.precioOferta}`;
-    container.innerHTML += `
 
-    <div class="card">
-      <!-- <h4 class="title">MacBook Pro</h4> -->
-      <img
-        src="${laptop.imagen}"
-        alt=""
-        
-      />
-      <div class="container-price">
-        <p class="name">${laptop.nombre} </p>
-        <p class="empres">${laptop.marca}</p>
-       <div class="box-price">
-         <p class="price"> ${laptop.precioNormal}</p>
-         <p class="sale-price">S/ ${laptop.precioOferta}</p>
-       </div>
+
+
+  let createCard=(array)=>{
+container.innerHTML=""
+
+if(array.length==0){
+  container.innerHTML="<h1 style='color:white'>No se encontro Resultados<h1/>"
+  return
+}
+    array.map((laptop) => {
+      // ahora en esta seccion vamos a crear los card
+      // Esto es JS se llama template string => ``
+      // Sirve para poder tener texto y variable juntos
+      // return `La laptop se llama ${laptop.nombre} y el precio es ${laptop.precioOferta}`;
+      container.innerHTML += `
+  
+      <div class="card">
+        <!-- <h4 class="title">MacBook Pro</h4> -->
+        <img class="ima_box"
+          src="${laptop.imagen}"
+          alt=""
+          
+        />
+        <div class="container-price">
+          <p class="name">${laptop.nombre} </p>
+          <p class="empres">${laptop.marca}</p>
+         <div class="box-price">
+           <p class="price"> ${laptop.precioNormal}</p>
+           <p class="sale-price">S/ ${laptop.precioOferta}</p>
+         </div>
+        </div>
+        <button class="btn-buy">Comprar</button>
       </div>
-      <button class="btn-buy">Comprar</button>
-    </div>
+  
+       `;
+    });
 
-     `;
-  });
+  }
+
+// 
+createCard(laptops);
+
+const modal_close=document.querySelector('.modal_close')
+const btm=document.getElementsByClassName("btn-buy")
+const modal=document.querySelector(".modal")
+const img=document.querySelector('.modal_img')
+const nombre=document.querySelector('.modal_title')
+
+const btnSearch=document.querySelector('.btn_search')
+const inputSearch=document.querySelector('.search')
+const clear=document.querySelector('.clear')
+
+clear.onclick=()=>{
+inputSearch.value=""
+  createCard(laptops)
+  fModal(btm)
+}
+
+
+btnSearch.onclick=()=>{
+
+const text=inputSearch.value
+
+if(text==""){
+  alert("campo vacio")
+  return
+}
+
+const filtro=laptops.filter(laptopp=>laptopp.nombre.toLowerCase().includes(text.toLowerCase())
+|| laptopp.marca.toLowerCase().includes(text.toLowerCase()))
+createCard(filtro)
+fModal(btm)
+
+}
+let img_array=[]
+
+const fModal=(arrayBtn)=>{
+
+  for(let a=0;a<arrayBtn.length;a++){
+    img_array[a]=document.getElementsByClassName('ima_box')[a].src
+    btm[a].onclick=(e)=>{
+      e.preventDefault();
+    
+      
+      modal.classList.add('modal_show')
+      img.src=`${img_array[a]}`;
+      nombre.textContent=`${laptops[a].nombre}`
+      console.log( "aaaaaaaa",img_array)
+    } 
+
+    }
+
+}
+
+// const ima=document.querySelector('.modal_img').src
+// console.log(" data",ima)
+
+fModal(btm)
+
+  // for(let a=0;a<btm.length;a++){
+  // btm[a].onclick=(e)=>{
+  //   e.preventDefault();
+  //   modal.classList.add('modal_show')
+  //   img.src=`${laptops[a].imagen}`
+  //   nombre.textContent=`${laptops[a].nombre}`
+  // }
+  // }
+
+  // modal.onclick=(e)=>{
+  //   e.preventDefault();
+  //   modal.classList.remove('modal_show')
+
+  // }
+  modal_close.onclick=(e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal_show')
+
+  }
+  console.log("asdasasd",btm)
+  // document.getElementById("myImageId").src="newSource.png";
+
+
